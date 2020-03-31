e213898668ca59df17a87541afa20040de5e9939
Omar Nabi <onabi20@gmail.com>
2020-03-31 02:35:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(numbers)
   square_array[number]
-  array.each do |number|
+  array.each do |num|
     square = num ** 2 
-    squared_array<< squared_array
+    squared_array << squared_array
   end
   squared_array
 end
